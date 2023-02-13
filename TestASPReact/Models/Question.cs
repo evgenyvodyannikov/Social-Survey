4a028230db248423f4bd7fd696cc9e5ef62d6f30
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-02-13 19:45:13 +0500

    create Question model

---
+++ TestASPReact/Models/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace TestASPReact.Models
+{
+    public class Question
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public Variant[] Variants { get; set; }
+    }
+}
---
+++ TestASPReact/Models/Variant.cs
@@ -0,0 +1,9 @@
+﻿namespace TestASPReact.Models
+{
+    public class Variant
+    {
+        public string Id { get; set; }
+        public string Body { get; set; } 
+        public bool Corret { get; set; } = false;
+    }
+}
