a476b3a3dd6618dad2e41a6f6df548dff27f6b7e
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-02-14 18:28:45 +0500

    define routes

---
+++ TestASPReact/Controllers/QuestionsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TestASPReact.Models;
+
+namespace TestASPReact.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuestionsController : ControllerBase
+    {
+
+        private readonly ILogger<QuestionsController> _logger;
+
+        public QuestionsController(ILogger<QuestionsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet, Route("getAll/")]
+        public IActionResult GetAll()
+        {
+            var questions = "ssssssssssss";
+            return Ok(questions);
+        }
+    }
+}
